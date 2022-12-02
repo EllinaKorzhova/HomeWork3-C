83734b7bab6d8fe6a5e0374f709abbe0b8fa0d3f
Ellina Korzhova <ellina.korzhpva@mail.ru>
2022-12-02 13:32:13 +0300

    Домашнее задание 3

---
+++ Example1/Program.cs
@@ -0,0 +1,15 @@
+﻿/*Напишите программу, которая принимает на вход пятизначное число и проверяет, является ли оно палиндромом.*/
+
+Console.WriteLine("Введите число");
+String number = Console.ReadLine();
+
+void CheckPalindrom(string num){
+if (num[0]==num[4] || number[1]==num[3]) {
+        Console.WriteLine($"Ваше число: {num} - палиндром.");
+  }
+  else Console.WriteLine($"Ваше число: {num} - НЕ палиндром.");
+}
+  if (number.Length == 5){
+    CheckPalindrom(number);
+  } else Console.WriteLine($"Введи правильное число");
+ 
---
+++ Example3/Program.cs
@@ -0,0 +1,14 @@
+﻿/*Напишите программу, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N.*/
+
+
+    Console.WriteLine("Введите число");
+    string numberStr = Console.ReadLine();
+    int number = int.Parse(numberStr);
+
+       if (number > 0) {
+   for (int i = 1; i <= number; i++ ){
+    Console.WriteLine($" {Math.Pow(i,3)}");
+}
+    } else {
+        Console.WriteLine("Вы ввели число меньше 1");
+    }
